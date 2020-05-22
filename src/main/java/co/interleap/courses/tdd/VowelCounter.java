276c78e61d61dd2606bc394cd05bf901fd15e876
Harsh Dubey <harsh.dubey@getzephyr.com>
2020-05-22 22:22:33 +0530

    first commit.

--- src/main/java/co/interleap/courses/tdd/VowelCounter.java
+++ src/main/java/co/interleap/courses/tdd/VowelCounter.java
@@ -1,4 +1,12 @@
 package co.interleap.courses.tdd;
 
+
 public class VowelCounter {
+
+    //private static final Set<String> VOWEL_SET = Stream.of("a", "e", "i" , "o", "u").collect(Collectors.toSet());
+
+    public int count(String text) {
+
+        return text.length();
+    }
 }
--- src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
+++ src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
@@ -1,9 +1,32 @@
 package co.interleap.courses.tdd;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class VowelCounterTest {
 
     @Test
     public void dummy(){}
+
+    @Test
+    public void testEmptyString() {
+        Assert.assertEquals(0,new VowelCounter().count(""));
+    }
+
+    @Test
+    public void shouldCountSingleVowelInString() {
+        VowelCounter vowelCounter = new VowelCounter();
+        Assert.assertEquals(1,vowelCounter.count("a"));
+        Assert.assertEquals(1,vowelCounter.count("e"));
+    }
+
+    @Test
+    public void shouldCountAllVowelsInStringHavingAllVowles() {
+        VowelCounter vowelCounter = new VowelCounter();
+        Assert.assertEquals(5,vowelCounter.count("aeiou"));
+        Assert.assertEquals(2,vowelCounter.count("ee"));
+        Assert.assertEquals(10,vowelCounter.count("iiiiiooooo"));
+        Assert.assertEquals(1,vowelCounter.count("o"));
+        Assert.assertEquals(3,vowelCounter.count("iou"));
+    }
 }
