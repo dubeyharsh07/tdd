e70e893774dae05c17467d4269b2162aff84c966
Harsh Dubey <harsh.dubey@getzephyr.com>
2020-05-23 00:00:33 +0530

    Updated tests scenario & code.

--- src/main/java/co/interleap/courses/tdd/VowelCounter.java
+++ src/main/java/co/interleap/courses/tdd/VowelCounter.java
@@ -1,12 +1,23 @@
 package co.interleap.courses.tdd;
 
 
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class VowelCounter {
 
-    //private static final Set<String> VOWEL_SET = Stream.of("a", "e", "i" , "o", "u").collect(Collectors.toSet());
+    private static final Set<String> VOWEL_SET = Stream.of("a", "e", "i" , "o", "u","A","E","I","O","U").collect(Collectors.toSet());
 
     public int count(String text) {
 
-        return text.length();
+        int counter = 0;
+        String[] arr = text.split("");
+        for(String letter : arr) {
+            if(VOWEL_SET.contains(letter)) {
+                counter += 1;
+            }
+        }
+        return counter;
     }
 }
--- src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
+++ src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
@@ -21,12 +21,28 @@ public class VowelCounterTest {
     }
 
     @Test
-    public void shouldCountAllVowelsInStringHavingAllVowles() {
+    public void shouldCountAllVowelsInStringHavingAllVowels() {
         VowelCounter vowelCounter = new VowelCounter();
         Assert.assertEquals(5,vowelCounter.count("aeiou"));
         Assert.assertEquals(2,vowelCounter.count("ee"));
         Assert.assertEquals(10,vowelCounter.count("iiiiiooooo"));
         Assert.assertEquals(1,vowelCounter.count("o"));
         Assert.assertEquals(3,vowelCounter.count("iou"));
+    }
+
+    @Test
+    public void shouldCountAllVowelsInStringHavingConsonants() {
+        VowelCounter vowelCounter = new VowelCounter();
+        Assert.assertEquals(1,vowelCounter.count("harsh"));
+        Assert.assertEquals(18, vowelCounter.count("write no production code except to pass a failing test."));
+    }
+
+    @Test
+    public void shouldCountAllVowelsInStringWithCaseSensitive() {
+        VowelCounter vowelCounter = new VowelCounter();
+        Assert.assertEquals(1,vowelCounter.count("harsh"));
+        Assert.assertEquals(1,vowelCounter.count("HARSH"));
+        Assert.assertEquals(12, vowelCounter.count("A Quick broWN FOX JUmped over the LAZY DOG."));
+
     }
 }
