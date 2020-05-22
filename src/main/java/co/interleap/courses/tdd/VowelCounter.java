d7710414036e6c44e013fa5e72bd5e5699c22815
Harsh Dubey <harsh.dubey@getzephyr.com>
2020-05-23 00:21:32 +0530

    Refactor part2

--- src/main/java/co/interleap/courses/tdd/VowelCounter.java
+++ src/main/java/co/interleap/courses/tdd/VowelCounter.java
@@ -1,27 +1,22 @@
 package co.interleap.courses.tdd;
 
 
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class VowelCounter {
-
-    private static final Set<String> VOWEL_SET = Stream.of("a", "e", "i" , "o", "u","A","E","I","O","U").collect(Collectors.toSet());
 
     public int count(String text) {
 
         int counter = 0;
-        String[] arr = text.split("");
-        for(String letter : arr) {
-            if(isAVowel(letter)) {
-                counter += 1;
+        if (text.length() > 0) {
+            for (String letter : text.split("")) {
+                if (isAVowel(letter)) {
+                    counter += 1;
+                }
             }
         }
         return counter;
     }
 
     private boolean isAVowel(String letter) {
-        return VOWEL_SET.contains(letter);
+        return "aeiouAEIOU".contains(letter);
     }
- }
+}
