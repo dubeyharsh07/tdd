bb735e793b708af57e71cd8acac7a4ad9ad6da58
Harsh Dubey <harsh.dubey@getzephyr.com>
2020-05-23 23:24:59 +0530

    refactored test methods & impls.

--- src/main/java/co/interleap/courses/tdd/InvoiceGenerator.java
+++ src/main/java/co/interleap/courses/tdd/InvoiceGenerator.java
@@ -3,11 +3,15 @@ package co.interleap.courses.tdd;
 
 public class InvoiceGenerator {
 
-    public int generateInvoice(Integer kms, Integer mins) {
+    public static final int FARE_PER_KM = 10;
+    public static final int FARE_PER_MIN = 1;
 
-        if(kms < 0 || mins < 0)
+    public double generateInvoice(double distance, double time) {
+
+        if(distance < 0 || time < 0)
             return 0;
 
-        return ((10 * kms) + (1 * mins));
+        return (FARE_PER_KM * distance + (FARE_PER_MIN * time));
     }
 }
+
--- src/test/java/co/interleap/courses/tdd/InvoiceGeneratorTest.java
+++ src/test/java/co/interleap/courses/tdd/InvoiceGeneratorTest.java
@@ -1,44 +1,40 @@
 package co.interleap.courses.tdd;
 
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class InvoiceGeneratorTest {
 
     @Test
-    public void shouldTestEmptyKilometersValue(){
-        Assert.assertEquals(0, new InvoiceGenerator().generateInvoice(0,0));
+    public void shouldTestEmptyDistanceValue(){
+        assertEquals(0, new InvoiceGenerator().generateInvoice(0,0),0.1);
     }
 
     @Test
-    public void shouldTestEmptyMinutesValue() {
-        Assert.assertEquals(0, new InvoiceGenerator().generateInvoice(0,0));
+    public void shouldTestEmptyTimeValue() {
+        assertEquals(0, new InvoiceGenerator().generateInvoice(0,0), 0.1);
     }
 
     @Test
-    public void shouldTestWithSingleValueForKilometers() {
-        Assert.assertEquals(11, new InvoiceGenerator().generateInvoice(1,1));
-        Assert.assertEquals(22, new InvoiceGenerator().generateInvoice(2,2));
+    public void shouldGenerateInvoiceForGivenDistanceAndTime() {
+        assertEquals(11, new InvoiceGenerator().generateInvoice(1,1), 0.1);
+        assertEquals(22, new InvoiceGenerator().generateInvoice(2,2), 0.1);
     }
 
     @Test
-    public void shouldTestWithLargeValueForKilometersAndMinutes() {
-        Assert.assertEquals(150, new InvoiceGenerator().generateInvoice(10,50));
+    public void shouldTestWithNegativeValueForDistance() {
+        assertEquals(0, new InvoiceGenerator().generateInvoice(-1,50),0.1);
     }
 
     @Test
-    public void shouldTestWithNegativeValueForKilometers() {
-        Assert.assertEquals(0, new InvoiceGenerator().generateInvoice(-1,50));
-    }
-
-    @Test
-    public void shouldTestWithNegativeValueForMinutes() {
-        Assert.assertEquals(0, new InvoiceGenerator().generateInvoice(10,-50));
+    public void shouldTestWithNegativeValueForTime() {
+        assertEquals(0, new InvoiceGenerator().generateInvoice(10,-50),0.1);
     }
 
     @Test
     public void shouldTestWithBothNegativeValue() {
-        Assert.assertEquals(0, new InvoiceGenerator().generateInvoice(-10,-50));
+        assertEquals(0, new InvoiceGenerator().generateInvoice(-10,-50),0.1);
     }
 }
